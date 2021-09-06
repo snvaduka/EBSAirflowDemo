a1c5b63b6faab7172bb459d33487622402b08e78
snvaduka <88087256+snvaduka@users.noreply.github.com>
2021-09-06 21:09:56 +0530

    Updated EBSDataLoadSchedule.py

--- dags/EBSDataLoadSchedule.py
+++ dags/EBSDataLoadSchedule.py
@@ -20,6 +20,6 @@ hdbsql_task = BashOperator(
     #Pass your task name
     task_id="Create_Table_Task", 
     #Pass your command
-    bash_command=' /usr/sap/hdbclient/hdbsql -n 9f85d8e4-ea13-43bd-93f0-1fbdb14c3bd0.hana.prod-eu10.hanacloud.ondemand.com:443 -u AIRFLOW_SCHEDULER -p Hanadb@123 start task 9265AF07FD7F474F9F4374D564865534.TASK_FDW_CURRENCY_ADJ_F', 
+    bash_command=' /opt/sap/hdbclient/hdbsql -n 9f85d8e4-ea13-43bd-93f0-1fbdb14c3bd0.hana.prod-eu10.hanacloud.ondemand.com:443 -u AIRFLOW_SCHEDULER -p Hanadb@123 start task 9265AF07FD7F474F9F4374D564865534.TASK_FDW_CURRENCY_ADJ_F', 
     dag=dag,
 )
